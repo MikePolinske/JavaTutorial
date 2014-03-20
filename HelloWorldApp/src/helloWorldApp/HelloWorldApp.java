1a6edd2f22ef1d904dcfea319eba2cefef020936
Mike Polinske <mikepolinske@yahoo.com>
2014-03-19 23:14:38 -0500

    Getting Started exercises
    Updated HelloWorldApp and added HelloWorldApp2

--- HelloWorldApp/src/helloWorldApp/HelloWorldApp.java
+++ HelloWorldApp/src/helloWorldApp/HelloWorldApp.java
@@ -6,6 +6,7 @@ package helloWorldApp;
  */
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+//        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hola Mundo!"); // Display the string.
     }
 }
